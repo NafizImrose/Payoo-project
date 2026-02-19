7d72659e0f5086d30e63711c21b1ce3f9221687c
NafizImrose <nafizimrose.official@gmail.com>
2026-02-19 14:35:50 +0600

    Update home page title and implement cashout functionality

--- script/home.js
+++ script/home.js
@@ -1,3 +1,16 @@
 document.getElementById("logout-btn").addEventListener("click", function () {
   window.location.replace("/index.html");
 });
+
+let flag = false;
+document.getElementById("add-money").addEventListener("click", function () {
+  const cashOut = document.getElementById("cashout-sec");
+  if (!flag) {
+    // cashOut.style.display = "block";
+    cashOut.classList.remove("hidden");
+    flag = true;
+  } else {
+    cashOut.classList.add("hidden");
+    flag = false;
+  }
+});
