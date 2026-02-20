83698616e27180cfa7b0d71598d184cf7bc3f7c4
NafizImrose <nafizimrose.official@gmail.com>
2026-02-20 17:20:14 +0600

    Implement add money and cashout functionality with validation and dynamic UI updates

--- script/home.js
+++ script/home.js
@@ -25,32 +25,70 @@ document.getElementById("logout-btn").addEventListener("click", function () {
 //   cashOut.classList.add("hidden");
 // });
 
-document.getElementById("withdraw-btn").addEventListener("click", function () {
-  const agent = document.getElementById("agent-number");
-  const agentNumber = agent.value;
-
-  const pin = document.getElementById("pin");
+//Machine ***************************************************************************
+function getPin(value) {
+  const pin = document.getElementById(value);
   const pinNumber = pin.value;
+  return pinNumber;
+}
+
+function inputAmount(value) {
+  const amount = document.getElementById(value);
+  const finalAmount = Number(amount.value);
+  return finalAmount;
+}
+
+function currentBalance() {
+  const balance = document.getElementById("balance");
+  let Amount = Number(balance.innerText);
+  return Amount;
+}
+
+function replaceBlance(value) {
+  balance.innerText = value;
+}
+
+function GetValueOfInput(value) {
+  const agent = document.getElementById(value);
+  const finalNumber = agent.value;
+  return finalNumber;
+}
+function showOnly(id) {
+  const addMoney = document.getElementById("add-money-sec");
+  const cashOut = document.getElementById("cashout-sec");
+
+  addMoney.classList.add("hidden");
+  cashOut.classList.add("hidden");
+
+  const select = document.getElementById(id);
+  select.classList.remove("hidden");
+}
+//Machine ******************************************************************************************
+
+// cash-out ****************************************************************************************
+document.getElementById("withdraw-btn").addEventListener("click", function () {
+  const agentNumber = GetValueInput("out-agent-number");
+
+  const pinNumber = getPin("cash-out-pin");
 
   if (agentNumber.startsWith("01") && pinNumber == "1234") {
-    const amount = document.getElementById("amount-number");
-    const withdrawAmount = Number(amount.value); //.value o string and inner text to string e , Number() diye numaric krte hoy
+    const withdrawAmount = inputAmount("out-amount-number"); //.value o string and inner text to string e , Number() diye numaric krte hoy
 
-    const balance = document.getElementById("balance");
-    let balanceAmount = Number(balance.innerText); // const diyechilam
+    let balanceAmount = currentBalance(); // const diyechilam
 
     if (withdrawAmount > balanceAmount) {
       alert("Insuffient Balance");
     } else {
       balanceAmount = balanceAmount - withdrawAmount;
-      balance.innerText = balanceAmount;
+      replaceBlance(balanceAmount);
     }
   } else {
     alert("Invalid pin or agent number");
   }
 });
+// cash-out-end ****************************************************************************************
 
-// Add-Money
+// Add-Money-start ***************************************************************************************
 // let flag1 = false;
 // const addBtn = document.getElementById("add-money");
 // const addMoney = document.getElementById("add-money-sec");
@@ -72,3 +110,26 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
 // addBtn.addEventListener("blur", function () {
 //   addMoney.classList.add("hidden");
 // });
+
+document.getElementById("add-btn").addEventListener("click", function () {
+  const agent = GetValueOfInput("banc-name");
+  if (agent == "Select A Bank") {
+    alert("Please select a bank");
+    return;
+  }
+  const bancAccNum = GetValueOfInput("add-agent-number");
+  if (bancAccNum.length < 11) {
+    alert("Invalid Account Number");
+    return;
+  }
+  if (getPin("add-pin") == "1234") {
+    let balance = currentBalance();
+    const inputBalance = inputAmount("add-amount-number");
+
+    balance += inputBalance;
+    replaceBlance(balance);
+  } else {
+    alert("Invalid Pin Number");
+  }
+});
+// Add-Money-end ***************************************************************************************
